0cd3d132b936c6561938a28af1a07cd7267813ce
Chris Cornutt <chris.cornutt@hp.com>
2014-10-16 10:38:05 -0500

    Adding a Client class (what makes the actual curl calls

---
+++ src/Yubikey/Client.php
@@ -0,0 +1,60 @@
+<?php
+//http://www.onlineaspect.com/2009/01/26/how-to-use-curl_multi-without-blocking/
+
+namespace Yubikey;
+
+class Client
+{
+    /**
+     * Send the request(s) via curl
+     *
+     * @param  array|\Yukikey\RequestCollection $requests Set of \Yubikey\Request objects
+     * @return \Yubikey\ResponseCollection instance
+     */
+    public function send($requests)
+    {
+        if (get_class($requests) !== 'Yubikey\\RequestCollection') {
+            $requests = new \Yubikey\RequestCollection($requests);
+        }
+
+        $multi = curl_multi_init();
+        $curls = array();
+        $content = array(
+            'response' => null,
+            'host' => null
+        );
+
+        // this should be a collection too
+        $responses = new \Yubikey\ResponseCollection();
+
+        foreach ($requests as $index => $request) {
+            $curls[$index] = curl_init();
+            curl_setopt_array($curls[$index], array(
+                CURLOPT_URL => $request->getUrl(),
+                CURLOPT_HEADER => 0,
+                CURLOPT_RETURNTRANSFER => 1
+            ));
+            curl_multi_add_handle($multi, $curls[$index]);
+        }
+
+        do {
+            while (($mrc = curl_multi_exec($multi, $active)) == CURLM_CALL_MULTI_PERFORM);
+            while ($info = curl_multi_info_read($multi)) {
+                if ($info['result'] == CURLE_OK) {
+                    $return = curl_multi_getcontent($info['handle']);
+                    $cinfo = curl_getinfo($info['handle']);
+                    $url = parse_url($cinfo['url']);
+
+                    // echo 'content: '.$return."\n";
+                    $response = new \Yubikey\Response(array(
+                        'host' => $url['host']
+                    ));
+                    $response->parse($return);
+                    $responses->add($response);
+                }
+            }
+        } while ($active);
+
+        return $responses;
+    }
+}
